d5f9448a2e68cb4ca4ef759643640b090cffb15d
Julien M <julien.marzal@telecom-paris.fr>
2020-12-16 10:48:48 +0100

    Menubar character selection and refresh functional

--- UI_classes.py
+++ UI_classes.py
@@ -7,23 +7,31 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title)
         self.characlist = []
+        self.selectedchar = None
 
         with open("characters", "rb") as file:
             self.characlist = Unpickler(file).load()
 
         self.CreateStatusBar()
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.menu = MainMenuBar()
+        self.menu = MainMenuBar(self)
         self.SetMenuBar(self.menu)
-#        self.Bind(wx.EVT_MENU, self.problem, id=0)
         self.Show(True)
 
     def get_characlist(self):
 
         return self.characlist
 
-    def problem(self, event):
-        print("ouch")
+    def get_selectedchar(self):
+
+        return self.selectedchar
+
+    def launch_charedit(self, index):
+
+        self.selectedchar = self.characlist[index]
+
+    def login(self):
+        pass
 
 
 class MainMenuBar(wx.MenuBar):
@@ -36,17 +44,35 @@ class MainMenuBar(wx.MenuBar):
         item = self.charmenu.Append(wx.ID_ANY, "Créer un personnage", "Ouvrir le menu de création des personnages")
         self.Bind(wx.EVT_MENU, self.goto_create, item)
         self.charmenu.AppendSeparator()
-
-        for charac in self.get_characlist():
-            item = self.charmenu.Append(wx.ID_ANY, charac.get_name(), "Afficher le personnage "+charac.get_name())
-            self.Bind(wx.EVT_MENU)
+        self.refresh()
 
     def get_characlist(self):
 
         return self.master.characlist
 
-    def goto_create(self, event=None):
-        print(self.get_characlist())
+    def goto_charedit(self, _event, index):
+
+        print("On part sur le perso", index)
+        self.master.launch_charedit(index)
+
+    def goto_create(self, _event=None):
+        self.refresh()
+
+    def goto_login(self,_event=None):
+        self.master.login()
+
+    def refresh(self):
+
+        for i in range(self.charmenu.GetMenuItemCount()-1, 1, -1):
+            item = self.charmenu.FindItemByPosition(i)
+            self.Unbind(wx.EVT_MENU, item)
+            self.charmenu.DestroyItem(item)
+
+        i = 0
+        for charac in self.get_characlist():
+            item = self.charmenu.Append(wx.ID_ANY, charac.get_name(), "Afficher le personnage " + charac.get_name())
+            self.Bind(wx.EVT_MENU, lambda event, temp=i: self.goto_charedit(event, temp), item)
+            i += 1
 
 
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
