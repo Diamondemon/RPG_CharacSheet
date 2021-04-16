16739f7b43257cdae4142a68bb54e5616cb158ab
Julien M <julien.marzal@telecom-paris.fr>
2021-04-16 23:38:29 +0200

    Added shadow version of CharUsefulFrame

---
+++ CUF.py
@@ -0,0 +1,19 @@
+import CNbk
+
+
+class CharUsefulFrame:
+    """ Fiche personnage résumée """
+
+    def refresh(self, event=None):
+        pass
+
+    def get_selectedchar(self):
+        return self.parent().get_selectedchar()
+
+    def parent(self) -> CNbk.CharNotebook:
+        """
+        Method called to get the parent widget (the Notebook)
+
+        :return: the reference to the parent
+        """
+        pass
